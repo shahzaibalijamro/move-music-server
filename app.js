c1977566f6ffe41d3167dc2d8d12505b58684929
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-02-21 17:10:50 +0500

    commands added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     credentials: true,
 }));
 export {app}
