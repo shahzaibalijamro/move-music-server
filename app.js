5b760c9aca0902b31e54d32ff81beb81264f4239
ShahzaibAliJamro <shahzaibalijamro@gmail.com>
2025-03-01 00:21:01 +0500

    commands added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://move-music-client.vercel.app',
     credentials: true,
 }));
 export {app}
